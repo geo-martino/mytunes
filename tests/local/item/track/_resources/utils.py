4f9e7520682deca4e8ed62989ffed6d1616d3441
geo-martino <gm.login+tech@pm.me>
2025-05-26 14:49:46 -0400

    drop track test resources
    Signed-off-by: geo-martino <gm.login+tech@pm.me>

--- tests/local/item/track/_resources/utils.py
+++
@@ -1,9 +0,0 @@
-import mimetypes
-from pathlib import Path
-
-TRACK_RESOURCE_PATH = Path(__file__).parent.joinpath("_resources")
-TRACK_PATHS = [
-    path for path in TRACK_RESOURCE_PATH.rglob("*")
-    if mimetypes.guess_type(path)[0].startswith("audio")
-]
-
