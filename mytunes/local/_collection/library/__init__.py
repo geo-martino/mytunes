4afc5697a7466330bc3317c51c698cfc0c88dade
George Marino <gm.engineer@pm.me>
2026-04-12 17:46:39 +0100

    move system paths to general import

--- mytunes/local/_collection/library/__init__.py
+++ mytunes/local/_collection/library/__init__.py
@@ -1,6 +1,11 @@
 from ._base import LocalLibrary
+from ._path import LocalSystemPath, LocalSystemPaths
 
-__all__ = [LocalLibrary.__name__]
+__all__ = [
+    LocalLibrary.__name__,
+    LocalSystemPath.__name__,
+    LocalSystemPaths.__name__,
+]
 
 # we must import all the supported formats here so that they are registered in the registry
 from .musicbee import MusicBee
