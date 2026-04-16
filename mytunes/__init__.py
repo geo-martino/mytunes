93f3fe54a9102b2b2ef99488d4149fbc3b76d5cf
George Marino <gm.engineer@pm.me>
2026-04-16 22:25:07 +0100

    fix warning suppress

--- mytunes/__init__.py
+++ mytunes/__init__.py
@@ -23,7 +23,7 @@ __all__ = [
 
 # WORKAROUND: get way too many warnings when trying to dump tracks, this stops them
 warnings.filterwarnings(
-    "ignore", module="pydantic", category=UserWarning,# message=".*serialized value may not be as expected.*"
+    "ignore", message=".*serialized value may not be as expected.*"
 )
 
 # we must import all the supported URI formats here so that they are registered in the registry
