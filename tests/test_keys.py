2580922b8905b58ae08869ad0bc07c7156e0395b
George Marino <gm.engineer@pm.me>
2026-04-30 01:04:41 +0100

    add requirement for common unique attributes

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -1,10 +1,19 @@
 import pytest
 
 from mytunes._base.resource import ResourceModel
-from mytunes.core._collection.playlist import RemotePlaylist, Playlist
-from mytunes.core._item.track import RemoteTrack, Track
-from mytunes.local._collection.playlist import LocalPlaylist
-from mytunes.local._item.track import LocalTrack
+from mytunes.core.genre import RemoteGenre
+from mytunes.core.album import RemoteAlbum
+from mytunes.core.artist import RemoteArtist
+from mytunes.core.genre import Genre
+from mytunes.core.playlist import RemotePlaylist, Playlist
+from mytunes.core.album import Album
+from mytunes.core.artist import Artist
+from mytunes.core.track import RemoteTrack, Track
+from mytunes.local.genre import LocalGenre
+from mytunes.local.artist import LocalArtist
+from mytunes.local.album import LocalAlbum
+from mytunes.local.playlist import LocalPlaylist
+from mytunes.local.track import LocalTrack
 
 
 @pytest.mark.parametrize(
@@ -12,18 +21,18 @@ from mytunes.local._item.track import LocalTrack
     [
         (LocalTrack, RemoteTrack),
         (LocalPlaylist, RemotePlaylist),
-        # known to not be supported for this
-        # (LocalAlbum, RemoteAlbum),
-        # (LocalArtist, RemoteArtist),
+        (LocalAlbum, RemoteAlbum),
+        (LocalArtist, RemoteArtist),
+        (LocalGenre, RemoteGenre),
     ]
 )
-def test_common_unique_attributes(source: ResourceModel, target: ResourceModel):
+def test_base_models_have_common_unique_attributes(source: ResourceModel, target: ResourceModel):
     """Needed to ensure all supported `merge_...` operations work correctly."""
     assert source.__unique_attributes__ & target.__unique_attributes__
 
 
-@pytest.mark.parametrize("model_type", [Track, Playlist])
-def test_registry_has_unique_attributes(model_type: type[ResourceModel]):
+@pytest.mark.parametrize("model_type", [Track, Playlist, Album, Artist, Genre])
+def test_final_models_have_common_unique_attributes(model_type: type[ResourceModel]):
     """Needed to ensure comparison across libraries work correctly."""
     assert all(kls.__unique_attributes__ for kls in model_type.registered_submodels)
 
